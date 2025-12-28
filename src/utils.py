c25672c9abed06833922d9130ff08d30d6abe870
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-28 21:33:23 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,10 @@
+import ast
+
+
+def get_node_name(node):
+    if isinstance(node, ast.FunctionDef):
+        return node.name
+    if isinstance(node, ast.ClassDef):
+        return node.name
+    return "unknown"
 
