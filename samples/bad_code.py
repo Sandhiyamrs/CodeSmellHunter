c0554d38e7e27cb24b166266a06c249f11159c3a
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-28 21:35:15 +0530

    Update bad_code.py

--- samples/bad_code.py
+++ samples/bad_code.py
@@ -1,1 +1,23 @@
+class BadExample:
+    def method1(self): pass
+    def method2(self): pass
+    def method3(self): pass
+    def method4(self): pass
+    def method5(self): pass
+    def method6(self): pass
+    def method7(self): pass
+    def method8(self): pass
+    def method9(self): pass
+    def method10(self): pass
+    def method11(self): pass
+
+
+def very_long_function(a, b, c, d, e, f):
+    x = 42
+    if a:
+        if b:
+            if c:
+                if d:
+                    if e:
+                        print(x)
 
