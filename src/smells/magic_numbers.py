2ec4874bf83d6901473607aedbf1d5c27d59119a
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-28 21:34:32 +0530

    Update magic_numbers.py

--- src/smells/magic_numbers.py
+++ src/smells/magic_numbers.py
@@ -1,1 +1,20 @@
+import ast
+
+
+class MagicNumberDetector:
+    def __init__(self, config):
+        self.ignore = config["IGNORE_MAGIC_NUMBERS"]
+
+    def detect(self, tree, file_path):
+        issues = []
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Constant) and isinstance(node.value, (int, float)):
+                if node.value not in self.ignore:
+                    issues.append({
+                        "type": "Magic Number",
+                        "file": file_path,
+                        "value": node.value,
+                        "line": node.lineno,
+                    })
+        return issues
 
