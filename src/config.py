c2813c7944d8a3c2f8fecdd97a0e37133521d76e
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-28 21:32:53 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,9 @@
 
+def load_config():
+    return {
+        "MAX_METHOD_LENGTH": 30,
+        "MAX_NESTING_DEPTH": 4,
+        "MAX_PARAMETERS": 5,
+        "MAX_CLASS_METHODS": 10,
+        "IGNORE_MAGIC_NUMBERS": {0, 1, -1},
+    }
