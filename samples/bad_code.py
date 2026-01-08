46bff55ce62ba817fc84796bababe57ddda3c008
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-08 23:06:01 +0530

    Update bad_code.py

--- samples/bad_code.py
+++ samples/bad_code.py
@@ -1,23 +1,28 @@
-class BadExample:
-    def method1(self): pass
-    def method2(self): pass
-    def method3(self): pass
-    def method4(self): pass
-    def method5(self): pass
-    def method6(self): pass
-    def method7(self): pass
-    def method8(self): pass
-    def method9(self): pass
-    def method10(self): pass
-    def method11(self): pass
+class DataProcessor:
 
+    def process_data(self, a, b, c, d, e, f, g):
+        total = 0
+        for i in range(10):
+            if i % 2 == 0:
+                if i > 3:
+                    if i != 7:
+                        total += 42
+        for j in range(20):
+            total += j
+        for k in range(30):
+            total += k
+        return total
 
-def very_long_function(a, b, c, d, e, f):
-    x = 42
-    if a:
-        if b:
-            if c:
-                if d:
-                    if e:
-                        print(x)
-
+    def m1(self): pass
+    def m2(self): pass
+    def m3(self): pass
+    def m4(self): pass
+    def m5(self): pass
+    def m6(self): pass
+    def m7(self): pass
+    def m8(self): pass
+    def m9(self): pass
+    def m10(self): pass
+    def m11(self): pass
+    def m12(self): pass
+    def m13(self): pass
